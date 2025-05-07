45417e3a33ca9ad662a6c33082210a8cd5fcaff6
FatemaTaher <125912196+FatemaTaher@users.noreply.github.com>
2025-05-07 19:55:51 +0300

    initial commit

---
+++ Fatema/src/test/java/com/eureka_server/Fatema/FatemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka_server.Fatema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
